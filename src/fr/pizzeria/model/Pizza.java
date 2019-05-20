a0e3555275697a4a5c03d4ef421e3bedb559b9d2
bilel <bilelh532@gmail.com>
2019-05-20 10:25:05 +0200

    TP1 + creation PizzaJdbcDao

---
+++ src/fr/pizzeria/model/Pizza.java
@@ -0,0 +1,46 @@
+package fr.pizzeria.model;
+
+import fr.pizzeria.utils.Rule;
+import fr.pizzeria.utils.ToString;
+
+public class Pizza {
+	
+	public int id ;
+	@ToString (uppercase = true)
+	public String code ;
+	@ToString (uppercase = false)
+	public String libelle ;
+	@Rule(min=0) 
+	public double prix ;
+	public static int id_pizza = 0 ;
+		// AJOUT DE L'ATTRIBUT DE TYPE CategoriePizza
+	public CategoriePizza cat;
+	
+		// AJOUT DE LA CATEGORIE DE PIZZA DANS LE CONSTRUCTEUR
+	public Pizza (String code , String libelle , double prix , CategoriePizza cat) {
+		this.id = id_pizza++ ;
+		this.code = code ;
+		this.libelle = libelle ;
+		this.prix = prix ;
+		this.cat = cat ;
+	}
+	
+		// AJOUT DE LA CATEGORIE DE PIZZA DANS LE CONSTRUCTEUR
+	public Pizza (int id , String code , String libelle , double prix , CategoriePizza cat) {
+		this.id = id ;
+		if (id > id_pizza) {
+			id_pizza = id;
+		}
+		this.code = code ;
+		this.libelle = libelle ;
+		this.prix = prix ;
+		this.cat = cat ;
+	}
+	
+	//public String toString () {
+		
+		
+	//}
+	
+
+}
